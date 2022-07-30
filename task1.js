3d28f81476901339e017ba61c001dcdca0dcce52
Berez0v <sasa59382@gmail.com>
2022-07-30 18:00:02 +0300

    task_#1

--- task1.js
+++ task1.js
@@ -1,31 +1,41 @@
 function digitToWord(n) {
     switch (n) {
         case 0:
-            console.log("zero")
-            case 1:
-            console.log("one")
-            case 2:
-            console.log("two")
-            case 3:
-            console.log("three")
-            case 4:
-            console.log("four")
-            case 5:
-                console.log("five")
-                case 6:
-            console.log("six")
-            case 7:
-            console.log("seven")
-            case 8:
-            console.log("eight")
-            case 9:
-            console.log("nine")
-        break;
+            console.log("zero");
+            break;
+        case 1:
+            console.log("one");
+            break;
+        case 2:
+            console.log("two");
+            break;
+        case 3:
+            console.log("three");
+            break;
+        case 4:
+            console.log("four");
+            break;
+        case 5:
+            console.log("five");
+            break;
+        case 6:
+            console.log("six");
+            break;
+        case 7:
+            console.log("seven");
+            break;
+        case 8:
+            console.log("eight");
+            break;
+        case 9:
+            console.log("nine");
+            break;
         default:
-           console.log(n+ " is not a digit");
+            console.log(n + " is not a digit");
     }
 }
 digitToWord(5);
-
-digitToWord("a");
+digitToWord("abc");
 digitToWord(10);
+digitToWord(-15);
+digitToWord(0);
