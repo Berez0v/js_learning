9ed4150e5f28c3cc3d8a66824b223728ac69a347
Berez0v <sasa59382@gmail.com>
2022-07-31 20:23:56 +0300

    sss

--- task9.js
+++
@@ -1,6 +0,0 @@
-let name_ = prompt("Your name:", "");
-alert(`Hello ${name_}! `);
-let isJsDev = confirm("Are you familiar with JS?");
-if (isJsDev) {
-    alert("Well done!");
-}else alert("You can start learning right now!");
