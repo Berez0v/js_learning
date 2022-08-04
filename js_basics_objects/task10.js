8b21ce3419b325bef4dd405fe123a7f4d6a46648
Berez0v <sasa59382@gmail.com>
2022-08-04 18:09:50 +0300

    task#10

--- js_basics_objects/task10.js
+++ js_basics_objects/task10.js
@@ -16,36 +16,17 @@ let Fedor = {
     contacts: {
         phone: `112`,
         email: `email@domain.com`,
-        valet: {
-            cas: "sfds",
-            ass: "dfsd"
-        }
     },
     address: `Moldova`
 }
 
 
 
-// function allValues(obj) {
-//     let rtn = Object.entries(obj);
-//     rtn.forEach((element, index) => {
-//         if (typeof element[1] === 'object') {
-//             let temp = allValues(element[1]);
-//             let temp1 =
-//                 element = temp;
-//             rtn.splice(index, 1);
-//             rtn.push(temp);
-//         }
-//     })
-
-//     return rtn;
-// }
-
 function allValues(obj) {
-    let rtn = Object.values(obj);
+    let rtn = Object.entries(obj);
     rtn.forEach((element, index) => {
-        if (typeof element === 'object') {
-            let temp = allValues(element);
+        if (typeof element[1] === 'object') {
+            let temp = allValues(element[1]);
             element = temp;
             rtn.push(temp);
         }
@@ -54,8 +35,51 @@ function allValues(obj) {
     return rtn;
 }
 
+function deleteObjects(arr) {
+    arr.forEach((element, index) => {
+        if (Array.isArray(element[1])) {
+            deleteObjects(element[1]);
+        }
+        if (typeof element[1] === "object" && !Array.isArray(element[1])) {
+            arr.splice(index, 1);
+        }
+    })
+}
+function searchIntersections(firstV, secondV) {
+    let rtn =[];
+    firstV.forEach((element, index) => {
+        if (Array.isArray(element[1])) {
+            rtn.push(searchIntersections(element, secondV[index]));
+
+        }
+        if (element[1] === secondV[index][1]) {
+            rtn.push(element);
+        }
+        
+    })
+    return rtn;
+}
+function returnValue(arr){
+    arr.forEach(element => {
+        if (Array.isArray(element[3])){
+            let temp = returnValue(element);
+            element = temp;
+            console.log(temp);
+      
+        }
+    })
+   return Object.fromEntries(arr);
+}
 function Intersection(first, second) {
-    console.log(allValues(first));
-    console.log(allValues(second));
+    let firstV = allValues(first);
+    let secondV = allValues(second);
+    deleteObjects(firstV);
+    deleteObjects(secondV);
+    // console.log(searchIntersections(firstV,secondV));
+  return  returnValue(searchIntersections(firstV,secondV));
+// return Object.fromEntries(searchIntersections(firstV,secondV));
+
 }
 Intersection(Vasil, Fedor);
+console.log(Intersection(Vasil, Fedor));
+//I don't know how it works,but it works!))))
