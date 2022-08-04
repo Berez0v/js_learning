edd840984aafb325f51186d9ddd67d88d4798970
Berez0v <sasa59382@gmail.com>
2022-08-04 22:31:37 +0300

    fix_task_7

--- js_basics_objects/task_7/task7.js
+++ js_basics_objects/task_7/task7.js
@@ -1,35 +1,37 @@
 let Person = {
     name: `Person`,
-    get firtstLetterFromName (){
+    get firtstLetterFromName() {
         return this.name[0];
     },
     age: 123,
-    get square_Age() {
+    get squareAge() {
         return this.age * this.age;
     },
     salary: 1542.33,
     get salaryNetto() {
         return this.salary * 0.87;
     },
+    get phoneWithCountryCode() {
+        return `+373-${this.contacts.phone}`
+    },
+    get personContacts() {
+        return `My email: ${this.contacts.email} | My phone-number: ${this.contacts.phone}`
+    },
     contacts: {
         phone: `112`,
-        get phoneWithCountryCode() { 
-          return `+373-${this.phone}`
-        },
+
         email: 'email@domain.com',
-        get personContacts () {
-            return `My email: ${this.email} | My phone-number: ${this.phone}`
-        }
+
     },
     address: `Moldova`,
-    get fullAdress(){
+    get fullAdress() {
         return `Chishinau,Republic of ${this.address}`
     }
 }
 console.log(Person.firtstLetterFromName);
-console.log(Person.square_Age);
+console.log(Person.squareAge);
 console.log(Person.salaryNetto);
-console.log(Person.contacts.phoneWithCountryCode);
-console.log(Person.contacts.personContacts);
+console.log(Person.phoneWithCountryCode);
+console.log(Person.personContacts);
 console.log(Person.fullAdress);
 
