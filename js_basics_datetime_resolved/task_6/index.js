c7cb613fbb320c1d5fe64d54abda62b478db240e
Berez0v <sasa59382@gmail.com>
2022-08-06 02:19:58 +0300

    task_6_fix

--- js_basics_datetime_resolved/task_6/index.js
+++ js_basics_datetime_resolved/task_6/index.js
@@ -2,6 +2,6 @@ function myFunction() {
     let birthDay = new Date(document.getElementById("birthDate").value);
     let currentDate = new Date();
     let difference = currentDate.getTime()-birthDay.getTime();
-    let text = `${Math.floor(difference/(1000*3600*24*365))} years ${Math.floor(difference/(1000*3600*24))} days ${Math.floor(difference/(1000*60))} minutes`;
+    let text = `${Math.floor(difference/(1000*3600*24*365))} years ${Math.floor(difference/(1000*3600*24*30.3))} months ${Math.floor(difference/(1000*3600*24))} days `;
     document.getElementById("Output").innerHTML = text;
   }
