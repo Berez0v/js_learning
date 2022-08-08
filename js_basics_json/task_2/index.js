25a2de48cc78aa9df357888ffdd659bba5533971
Berez0v <sasa59382@gmail.com>
2022-08-08 10:39:16 +0300

    task_2

---
+++ js_basics_json/task_2/index.js
@@ -0,0 +1,9 @@
+const power = {
+    voltage: 250,
+    current: 12
+}
+console.log(power);
+console.log(typeof power);
+let powerInJson = JSON.stringify(power);
+console.log(powerInJson);
+console.log(typeof powerInJson);
