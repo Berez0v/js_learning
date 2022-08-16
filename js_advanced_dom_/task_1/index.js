5817fbe55f3e43b0ce815429e97bff7891511920
Berez0v <sasa59382@gmail.com>
2022-08-17 00:01:05 +0300

    task_1-4

---
+++ js_advanced_dom_/task_1/index.js
@@ -0,0 +1,36 @@
+
+function changeColour() {
+    document.body.style.background = 'orange'
+}
+
+function appendChildLi() {
+    //else clearInterval(ListInterval);
+}
+
+function mainFunction() {
+    let myBody = document.getElementById('main_content')
+    myBody.style.background = 'red';
+    setTimeout(() => myBody.style.background = 'green', 3000);
+   
+    let my_Ol = document.createElement('ol');
+    my_Ol.id = 'list';
+    myBody.appendChild(my_Ol);
+
+
+    var ListInterval = setInterval(() => {
+        my_Ol = document.getElementById('list')
+        if (my_Ol.children.length < 20) {
+            let newLi = document.createElement('li');
+            if(my_Ol.children.length%2 === 0){
+                newLi.style.background = 'purple'; 
+            }else   newLi.style.background = 'lime'; 
+
+            newLi.textContent = `element ${my_Ol.children.length + 1}`
+            my_Ol.appendChild(newLi);
+        } else clearInterval(ListInterval);
+
+    }, 2000);
+}
+document.addEventListener("DOMContentLoaded", mainFunction);
+let myButton = document.getElementById('or_btn');
+myButton.addEventListener('click', changeColour);
